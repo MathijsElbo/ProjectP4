5c6c2caed21b90eaff7a44ce9be886ca3930ab62
Steven Li <327068@student.mboutrecht.nl>
2020-06-07 15:13:53 +0200

    Creates css for navbar basket icon

--- layout-basic/navbar.php
+++ layout-basic/navbar.php
@@ -1,7 +1,6 @@
 <?php $content = (isset($_GET['content']) ? $_GET['content'] : false); ?>
-<nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container">
-    <div class="row">
+<nav class="navbar navbar-expand-lg navbar-light navbar-sticky">
+
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -22,13 +21,12 @@
         </ul>
       </div>
       <div class="nav-icons">
-        <a href="index.php?content=basket"><i class="fas fa-shopping-cart"></i></a>
+        <a href="index.php?content=basket"><i class="fas fa-shopping-cart"><span class="display-cart-amount">1</span></i></a>
         <a href="index.php?content=redirect"><i class="fas fa-user"></i></a>
         <?php if (isset($_SESSION["id"])) {
             echo "<a href='index.php?content=uitloggen'><i class='fas fa-sign-out-alt'></i></a>";
           } 
         ?>
       </div>
-    </div>
-  </div>
+
 </nav>
