0e32929c0c472dbcdb288af4e4dd7ca6db873adb
Mathijs Elbertsen <328184@student.mboutrecht.nl>
2020-06-22 09:49:14 +0200

    home, over ons & contact contents af

---
+++ layout-content/contact.php
@@ -0,0 +1,37 @@
+<div class="container-fluid">
+    <div class="row content">
+        <div class="col-8">
+            <h2>Contact PVH</h2>
+            <table class="table table-striped">
+                <tbody>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>Website</td>
+                        <td>Adres</td>
+                        <td>Postcode</td>
+                        <td>Land</td>
+                        <td>Telefoonnummer</td>
+                        <td>Email</td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>postershop.nl</td>
+                        <td>Den Heuvel 62</td>
+                        <td>6881 VE Velp</td>
+                        <td>Nederland</td>
+                        <td>Tel. (026) 3512512</td>
+                        <td>info@postershop.nl</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-4">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2456.6874411767253!2d5.972447515567574!3d51.99435028280329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7a3817d9a8e41%3A0x551c92c31657a5b6!2sDen%20Heuvel%2062%2C%206881%20VE%20Velp!5e0!3m2!1snl!2snl!4v1581944561578!5m2!1snl!2snl"
+                width="100%" height="100%" frameborder="0" style="border:0;"></iframe>
+        </div>
+    </div>
+    <div class="row">\
+        <br>
+    </div>
+</div>
--- layout-content/overons.php
+++ layout-content/overons.php
@@ -3,12 +3,69 @@
     <div class="col-10 offset-1">
       <div class="jumbotron jumbotron-fluid">
         <div class="container">
-          <h1 class="display-4 text-center">over ons</h1>
           <div class="row">
-              <div class="col-4"></div>
-              <div class="col-4"></div>
-              <div class="col-4"></div>
-          </div>>
+            <div class="col-9">
+              <h1 class="display-4">over ons.</h1>
+              <p class="lead">Postershop.nl al XX jaar verkoper van de mooiste posters.
+              </p>
+              <hr class="my-4">
+              <p>We zijn al jaren de meeste vertrouwde posterwinkel in XXXXXXX Bestel en maak u woning compleet!
+              </p>
+            </div>
+            <div class="col-3">
+              <img src="/assets/img/opmaak1.png" alt="">
+            </div>
+          </div>
+          <div class="row">
+            <div class="card cardemp col-12">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="/assets/img/user.png" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Aart Anderson</h5>
+                    <p class="card-text">CEO van postershop.nl met jaren ervaring in het vak</p>
+                    <p class="card-text"><small class="text-muted">CEO</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br>
+          <div class="row">
+            <div class="card cardemp col-12">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="/assets/img/user.png" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Bea Buizinga</h5>
+                    <p class="card-text">Jaren ervaring in de marketing sector</p>
+                    <p class="card-text"><small class="text-muted">head of marketing</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <br>
+          <div class="row">
+            <div class="card cardemp col-12">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="/assets/img/user.png" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Charles Xavier</h5>
+                    <p class="card-text">PR expert met vele jaren ervaringS</p>
+                    <p class="card-text"><small class="text-muted">head of PR</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
