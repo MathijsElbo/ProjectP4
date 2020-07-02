06e34ca1cb0bb90b43fbad02a44737cfe7f2c437
Mathijs Elbertsen <328184@student.mboutrecht.nl>
2020-07-02 08:52:38 +0200

    informatie pagina correct toegevoegd

---
+++ layout-content/informatie.php
@@ -0,0 +1,41 @@
+<div class="container-fluid">
+    <div class="row content">
+        <div class="col-10 offset-1">
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-9">
+                            <h1 class="display-4">over ons.</h1>
+                            <p class="lead">Postershop.nl al XX jaar verkoper van de mooiste posters.
+                            </p>
+                            <hr class="my-4">
+                            <p>bestel voor 23.59 en ontvang de volgende dag u bestelling!
+                            </p>
+                        </div>
+                        <div class="col-3">
+                            <img src="/assets/img/opmaak2.png" alt="" height="100%" width="100%">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="card cardemp col-12">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="/assets/img/postnl.png" class="card-img" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Wie onze bezorger, postNL is</h5>
+                                        <p class="card-text">postNL, Dé dienstverlener in post en logistiek</p>
+                                        <p class="card-text"><small class="text-muted">Ze bezorgen dagelijks pakketten en vijf dagen per week post. 
+                                            Op een doordeweekse dag bezorgen ze in de Benelux gemiddeld 900.000 pakketten en 6,8 miljoen brieven. 
+                                            Voor hun klanten zijn zij de onmisbare schakel tussen verzenders en ontvangers en de verbinder tussen de fysieke en digitale wereld.</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
